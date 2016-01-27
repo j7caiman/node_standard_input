5f50501081c05e123e6db103c0f733817ffa5f38
jon <j7caiman@users.noreply.github.com>
2016-01-27 20:15:07 +0100

    add integer parsing, also repeat question if no input received

--- index.js
+++ index.js
@@ -8,19 +8,38 @@ function query(question) {
     terminal: false
   });
 
-  var result;
+  function askQuestion() {
+    var result;
+    rl.question(question, function(input) {
+      result = input;
+    });
 
-	rl.question(question, function (input) {
-    result = input;
-  });
+    while (result === undefined) {
+      deasync.runLoopOnce();
+    }
 
-  while(result === undefined) {
-    deasync.runLoopOnce();
+    return result;
+  }
+
+  var result = "";
+  while (result.length === 0) {
+    result = askQuestion();
   }
 
   rl.close();
 
-  return result;
+  if (isInteger(result)) {
+    return parseInt(result);
+  } else {
+    return result;
+  }
+}
+
+function isInteger(value) {
+  return !/[a-z]/i.test(value)
+    && !isNaN(value)
+    && parseInt(Number(value)) == value
+    && !isNaN(parseInt(value, 10));
 }
 
 module.exports = query;
